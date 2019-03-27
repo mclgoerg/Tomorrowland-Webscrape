b8e8ff4a73605b22310ffeb23a375756d2439282
mclgoerg <marcelgoergens@gmx.de>
2019-03-27 21:37:12 +0100

    Improved performance and some bug fixes

--- scraper.py
+++ scraper.py
@@ -5,16 +5,18 @@ from bs4 import BeautifulSoup as soup
 
 my_url = 'https://www.tomorrowland.com/en/festival/line-up/stages/friday-19-july-2019'
 
-# opening connection, grabbing the page 
+# opening connection, grabbing the page
 uClient = uReq(my_url)
-page_html= uClient.read()
+page_html = uClient.read()
 uClient.close()
 
 # html parsing
 page_soup = soup(page_html, "html.parser")
 
-containers = page_soup.findAll("div", {"class":"stage__content"})
-eventdays = page_soup.findAll("div", {"class":"eventdays"})
+
+eventdays = page_soup.findAll("div", {"class": "eventday"})
+#containers = eventdays[0].findAll("div", {"class": "stage__content"})
+#stages = page_soup.findAll("div", {"class": "stage__content"})
 
 ###
 # Stage Title
@@ -27,7 +29,6 @@ eventdays = page_soup.findAll("div", {"class":"eventdays"})
 #for artist in containers[0].findAll("li"):
 #	print(artist.text)
 
-#div_info = containers[0].find("div","item-info")
 
 #filename = "products.csv"
 #f = open(filename, "w")
@@ -37,26 +38,42 @@ eventdays = page_soup.findAll("div", {"class":"eventdays"})
 #f.write(headers)
 artist_count = 0
 i = 0
-for container in containers:
-    n = 0
-    stage_title = containers[i].find("div","stage__title").h5.text
-    print("Stage Title: "+ stage_title)
-
-    stage_location = containers[i].find("div","stage__title").p.text
-    print("Stage Location: "+ stage_location)
-
-    for artist in containers[i].findAll("li"):
-        print(containers[i].findAll("li")[n].text.strip())
-        n += 1
-        artist_count += 1
+day_number = 0
+weekend_number = 0
 
 
-	#f.write(brand + "," + product_name.replace(",", "|") + "," + shipping + "\n")
-    
-    i += 1
-	#brand = div_info.div.a.img["title"]
+for eventday in eventdays:
+    # Get Weekend
+    day_number += 1
+    if(day_number <= int(len(page_soup.findAll("div", {"class": "eventday"}))/2)):
+        print(int(len(page_soup.findAll("div", {"class": "eventday"}))/2))
+        weekend_number = 1
+    else:
+        print(int(len(page_soup.findAll("div", {"class": "eventday"}))/2))
+        weekend_number = 2
+
+    for stage in eventday.findAll("div","stage__content"):
+        n = 0
+        #stage_title = stages[i].find("div", "stage__title").h5.text
+        #print("Stage Title 2 : " + stage_title)
+        print("Stage Title: " + stage.h5.text)        
+
+        #stage_location = stages[i].find("div", "stage__title").p.text
+        #print("Stage Location: " + stage_location)
+        print("Stage Location: " + stage.p.text)
+        print("i ist: ", i)
+
+        
+        for artist in stage.findAll("li"):
+            print("Weekend: ", weekend_number)
+            print(artist.text.strip())
+
+            #print(containers[i].findAll("li")[n].text.strip())
+            n += 1
+            artist_count += 1
+
+        i += 1
+
 #f.close()
 
 print("Artist count: ", artist_count)
-#for eventday in eventdays:
-	#date = 
